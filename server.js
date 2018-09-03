2db70f8dcf828869a6e5cfb4c0ba9839a9807459
smibbie <35998407+smibbie@users.noreply.github.com>
2018-09-02 22:28:05 -0400

    enforcing https, adding redirect for heroku

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const app = express();
 // Set up static content ---------------------
 app.use(express.static("public"));
 
+// Force HTTPS redirect ----------------------
+// At the top, with other redirect methods before other routes
+app.get('*', (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] != 'https') {
+    res.redirect('https://britneyaronson.com' + req.url);
+  } else {
+    next(); // Continue to other routes if we're not redirecting
+  }
+});
+
 // Routes -------------------------------------
 const htmlRoutes = require("./routes/htmlRoutes.js");
 
