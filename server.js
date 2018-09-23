19ba878c6fd37518b7470f6f3381443183217243
smibbie <35998407+smibbie@users.noreply.github.com>
2018-09-23 13:30:07 -0400

    changed http handler back

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 
 // Force SSL/HTTPS redirect ----------------------
 // Redirect for "/" route only
-app.get("*", (req, res, next) => {
+app.get("/", (req, res, next) => {
   if (req.headers['X-Forwarded-Proto'] !== 'https') {
     res.redirect(`https://${req.headers.host}${req.url}`);
   } else {
